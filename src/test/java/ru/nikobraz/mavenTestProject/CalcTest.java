d0fc069dd7f87c2ff6e37eb940c5e4369548bdcf
nikobraz <nikita_yarushin@mail.ru>
2021-01-25 16:19:29 +0300

    Update to Junit5

--- src/test/java/ru/nikobraz/mavenTestProject/CalcTest.java
+++ src/test/java/ru/nikobraz/mavenTestProject/CalcTest.java
@@ -1,9 +1,10 @@
 package ru.nikobraz.mavenTestProject;
 
 import io.qameta.allure.Owner;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalcTest {
 
